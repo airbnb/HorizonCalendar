dbcac1a06aec7d3a85abe7d6b62072f2f7f5631a
Bryan Keller <kellerbryan19@gmail.com>
2023-09-18 15:20:52 -0400

    Add Swift linting

--- Package.swift
+++ Package.swift
@@ -1,25 +1,25 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.8
 
 import PackageDescription
 
 let package = Package(
-    name: "HorizonCalendar",
-    platforms: [
-        .iOS(.v11)
-    ],
-    products: [
-        .library(name: "HorizonCalendar", targets: ["HorizonCalendar"])
-    ],
-    targets: [
-        .target(
-            name: "HorizonCalendar",
-            path: "Sources"
-        ),
-        .testTarget(
-            name: "HorizonCalendarTests",
-            dependencies: ["HorizonCalendar"],
-            path: "Tests"
-        )
-    ],
-    swiftLanguageVersions: [.v5]
-)
+  name: "HorizonCalendar",
+  platforms: [
+    .iOS(.v11),
+  ],
+  products: [
+    .library(name: "HorizonCalendar", targets: ["HorizonCalendar"]),
+  ],
+  dependencies: [
+    .package(url: "https://github.com/airbnb/swift", .upToNextMajor(from: "1.0.1")),
+  ],
+  targets: [
+    .target(
+      name: "HorizonCalendar",
+      path: "Sources"),
+    .testTarget(
+      name: "HorizonCalendarTests",
+      dependencies: ["HorizonCalendar"],
+      path: "Tests"),
+  ],
+  swiftLanguageVersions: [.v5])
