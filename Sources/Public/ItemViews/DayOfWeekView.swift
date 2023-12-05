efc5fbffac1f1a7355ac6542be8bc3a7f4a767d5
Anton Redkozubov <39982533+antonredkozubov@users.noreply.github.com>
2023-12-05 17:04:11 +0300

    Update DayOfWeekView.swift

--- Sources/Public/ItemViews/DayOfWeekView.swift
+++ Sources/Public/ItemViews/DayOfWeekView.swift
@@ -27,6 +27,7 @@ public final class DayOfWeekView: UIView {
 
     backgroundLayer = CAShapeLayer()
     let backgroundShapeDrawingConfig = invariantViewProperties.backgroundShapeDrawingConfig
+    backgroundLayer.backgroundColor = UIColor.globalBackgroundColor.cgColor
     backgroundLayer.fillColor = UIColor.globalBackgroundColor.cgColor
     backgroundLayer.strokeColor = backgroundShapeDrawingConfig.borderColor.cgColor
     backgroundLayer.lineWidth = backgroundShapeDrawingConfig.borderWidth
