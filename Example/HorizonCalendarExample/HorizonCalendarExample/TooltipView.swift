70f379dc1186805ecfef886754471dc943f507c9
swiftlydesigner <98251445+swiftlydesigner@users.noreply.github.com>
2025-03-02 14:56:20 -0800

    Fix TooltipView Spacing

--- Example/HorizonCalendarExample/HorizonCalendarExample/TooltipView.swift
+++ Example/HorizonCalendarExample/HorizonCalendarExample/TooltipView.swift
@@ -14,119 +14,119 @@
 // limitations under the License.
 
 import HorizonCalendar
+import SwiftUICore
 import UIKit
 
 // MARK: - TooltipView
 
 final class TooltipView: UIView {
+    // MARK: Lifecycle
 
-  // MARK: Lifecycle
+    fileprivate init(invariantViewProperties: InvariantViewProperties) {
+        backgroundView = UIView()
+        backgroundView.backgroundColor = invariantViewProperties.backgroundColor
+        backgroundView.layer.borderColor = invariantViewProperties.borderColor.cgColor
+        backgroundView.layer.borderWidth = 1
+        backgroundView.layer.cornerRadius = 6
 
-  fileprivate init(invariantViewProperties: InvariantViewProperties) {
-    backgroundView = UIView()
-    backgroundView.backgroundColor = invariantViewProperties.backgroundColor
-    backgroundView.layer.borderColor = invariantViewProperties.borderColor.cgColor
-    backgroundView.layer.borderWidth = 1
-    backgroundView.layer.cornerRadius = 6
+        label = UILabel()
+        label.font = invariantViewProperties.font
+        label.textAlignment = invariantViewProperties.textAlignment
+        label.lineBreakMode = .byTruncatingTail
+        label.textColor = invariantViewProperties.textColor
 
-    label = UILabel()
-    label.font = invariantViewProperties.font
-    label.textAlignment = invariantViewProperties.textAlignment
-    label.lineBreakMode = .byTruncatingTail
-    label.textColor = invariantViewProperties.textColor
+        super.init(frame: .zero)
 
-    super.init(frame: .zero)
-
-    isUserInteractionEnabled = false
-    addSubview(backgroundView)
-    addSubview(label)
-  }
-
-  required init?(coder _: NSCoder) {
-    fatalError("init(coder:) has not been implemented")
-  }
-
-  // MARK: Internal
-
-  override func layoutSubviews() {
-    super.layoutSubviews()
-
-    guard let frameOfTooltippedItem else { return }
-
-    label.sizeToFit()
-    let labelSize = CGSize(
-      width: min(label.bounds.size.width, bounds.width),
-      height: label.bounds.size.height)
-
-    let backgroundSize = CGSize(width: labelSize.width + 16, height: labelSize.height + 16)
-
-    let proposedFrame = CGRect(
-      x: frameOfTooltippedItem.midX - (backgroundSize.width / 2),
-      y: frameOfTooltippedItem.minY - backgroundSize.height - 4,
-      width: backgroundSize.width,
-      height: backgroundSize.height)
-
-    let frame: CGRect
-    if proposedFrame.maxX > bounds.width {
-      frame = proposedFrame.applying(.init(translationX: bounds.width - proposedFrame.maxX, y: 0))
-    } else if proposedFrame.minX < 0 {
-      frame = proposedFrame.applying(.init(translationX: -proposedFrame.minX, y: 0))
-    } else {
-      frame = proposedFrame
+        isUserInteractionEnabled = false
+        addSubview(backgroundView)
+        addSubview(label)
     }
 
-    backgroundView.frame = frame
-    label.center = backgroundView.center
-  }
-
-  // MARK: Fileprivate
-
-  fileprivate var frameOfTooltippedItem: CGRect? {
-    didSet {
-      guard frameOfTooltippedItem != oldValue else { return }
-      setNeedsLayout()
+    @available(*, unavailable)
+    required init?(coder _: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
-  }
 
-  fileprivate var text: String {
-    get { label.text ?? "" }
-    set { label.text = newValue }
-  }
+    // MARK: Internal
 
-  // MARK: Private
+    override func layoutSubviews() {
+        super.layoutSubviews()
 
-  private let backgroundView: UIView
-  private let label: UILabel
+        guard let frameOfTooltippedItem else { return }
 
+        label.sizeToFit()
+        let labelSize = CGSize(
+            width: min(label.bounds.size.width, bounds.width),
+            height: label.bounds.size.height
+        )
+
+        let backgroundSize = CGSize(width: labelSize.width + 16, height: labelSize.height + 16)
+
+        let proposedFrame = CGRect(
+            x: frameOfTooltippedItem.midX - (backgroundSize.width / 2),
+            y: frameOfTooltippedItem.minY - backgroundSize.height - 4,
+            width: backgroundSize.width,
+            height: backgroundSize.height
+        )
+
+        let frame: CGRect = if proposedFrame.maxX > bounds.width {
+            proposedFrame.applying(.init(translationX: bounds.width - proposedFrame.maxX, y: 0))
+        } else if proposedFrame.minX < 0 {
+            proposedFrame.applying(.init(translationX: -proposedFrame.minX, y: 0))
+        } else {
+            proposedFrame
+        }
+
+        backgroundView.frame = frame
+        label.center = backgroundView.center
+    }
+
+    // MARK: Fileprivate
+
+    fileprivate var frameOfTooltippedItem: CGRect? {
+        didSet {
+            guard frameOfTooltippedItem != oldValue else { return }
+            setNeedsLayout()
+        }
+    }
+
+    fileprivate var text: String {
+        get { label.text ?? "" }
+        set { label.text = newValue }
+    }
+
+    // MARK: Private
+
+    private let backgroundView: UIView
+    private let label: UILabel
 }
 
 // MARK: CalendarItemViewRepresentable
 
 extension TooltipView: CalendarItemViewRepresentable {
+    struct InvariantViewProperties: Hashable {
+        var backgroundColor = UIColor.white
+        var borderColor = UIColor.black
+        var font = UIFont.systemFont(ofSize: 16)
+        var textAlignment = NSTextAlignment.center
+        var textColor = UIColor.black
+    }
 
-  struct InvariantViewProperties: Hashable {
-    var backgroundColor = UIColor.white
-    var borderColor = UIColor.black
-    var font = UIFont.systemFont(ofSize: 16)
-    var textAlignment = NSTextAlignment.center
-    var textColor = UIColor.black
-  }
+    struct Content: Equatable {
+        let frameOfTooltippedItem: CGRect?
+        let text: String
+        let customView: UIView
+    }
 
-  struct Content: Equatable {
-    let frameOfTooltippedItem: CGRect?
-    let text: String
-  }
+    static func makeView(
+        withInvariantViewProperties invariantViewProperties: InvariantViewProperties)
+        -> TooltipView
+    {
+        TooltipView(invariantViewProperties: invariantViewProperties)
+    }
 
-  static func makeView(
-    withInvariantViewProperties invariantViewProperties: InvariantViewProperties)
-    -> TooltipView
-  {
-    TooltipView(invariantViewProperties: invariantViewProperties)
-  }
-
-  static func setContent(_ content: Content, on view: TooltipView) {
-    view.frameOfTooltippedItem = content.frameOfTooltippedItem
-    view.text = content.text
-  }
-
+    static func setContent(_ content: Content, on view: TooltipView) {
+        view.frameOfTooltippedItem = content.frameOfTooltippedItem
+        view.text = content.text
+    }
 }
