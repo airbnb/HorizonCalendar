d768da43e1dbaad9e0ac77ab4729302caaa6cd1c
Cal Stephens <cal@calstephens.tech>
2025-12-20 08:49:41 -0800

    Update to latest version of Airbnb Swift Style Guide

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     .library(name: "HorizonCalendar", targets: ["HorizonCalendar"]),
   ],
   dependencies: [
-    .package(url: "https://github.com/airbnb/swift", .upToNextMajor(from: "1.0.1")),
+    .package(url: "https://github.com/airbnb/swift", .upToNextMajor(from: "1.2.0")),
   ],
   targets: [
     .target(
